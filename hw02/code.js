aee8b7dc9bb14b56e8477a4f33f18587797b0b77
acp9191 <averypeterson115@gmail.com>
2019-01-16 16:13:06 +0000

    HW02 files

---
+++ hw02/code.js
@@ -0,0 +1,71 @@
+(function() {
+  "use strict";
+
+  var firstNum;
+  var secondNum;
+  var operator;
+  var outputNum;
+  var output = document.getElementById('output');
+
+  function init() {
+    clearVars();
+
+    var nums = document.getElementsByClassName('btn-num');
+    var ops = document.getElementsByClassName('op');
+    var clear = document.getElementById('clear');
+
+    for (var idx = 0; idx < nums.length; idx++) {
+      nums[idx].addEventListener('click', handleNumClick);
+    }
+
+    for (var idx2 = 0; idx2 < ops.length; idx2++) {
+      ops[idx2].addEventListener('click', handleOpClick);
+    }
+    
+    clear.addEventListener('click', () => {
+      clearVars();
+      output.innerText = outputNum;
+    });
+  }
+  
+  function clearVars() {
+    firstNum = '';
+    secondNum = '';
+    operator = '';
+    outputNum = '0';
+  }
+
+
+  function handleNumClick(e) {
+    (outputNum === '0' && e.target.innerText != '.') ? outputNum = e.target.innerText : outputNum += e.target.innerText;
+    output.innerText = outputNum;
+  }
+
+  function handleOpClick(e) {
+    if (operator && e.target.classList.contains('equals')) {
+      secondNum = parseFloat(output.innerText);
+
+      output.innerText = ((operator) => { 
+        switch (operator) {
+          case 'x':
+            return firstNum * secondNum;
+          case '/':
+            return firstNum / secondNum;
+          case '-':
+            return firstNum - secondNum;
+          default:
+            return firstNum + secondNum;
+        }
+      })(operator);
+    
+      clearVars();
+
+    } else {
+      firstNum = parseFloat(output.innerText);
+      operator = e.target.innerText;
+      outputNum = '0';
+    }  
+  }
+
+  window.addEventListener('load', init, false);
+})();
